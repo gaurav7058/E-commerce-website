bf986887129c284b13aacae87c0f3856da7b95b8
Create_first_repo <gauravchoundiye123@gmail.com>
2025-06-01 18:58:04 +0530

    final

--- Admin/frontend_admin/src/App.jsx
+++ Admin/frontend_admin/src/App.jsx
@@ -8,7 +8,7 @@ import { Route, Routes } from 'react-router-dom'
 import Login from './components/Login'
 import { ToastContainer } from 'react-toastify';
 import Users from './pages/Users'
-export const backendUrl ="https://e-commerce-website-backend-vjs4.onrender.com";
+export const backendUrl ="https://e-commerce-website-backend-vjs4.onrender.com/admin";
 export const currency='$'
 const App = () => {
   const initialToken = localStorage.getItem('token') || '';
@@ -30,10 +30,10 @@ const App = () => {
         
         <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
         <Routes>
-          <Route path='/admin' element={<Users token={token}/>}/>
-          <Route path='/admin/add' element={<Add token={token}/>}/>
-          <Route path='/admin/list' element={<List token={token}/>}/>
-          <Route path='/admin/orders' element={<Orders token={token}/>}/>
+          <Route path='/' element={<Users token={token}/>}/>
+          <Route path='/add' element={<Add token={token}/>}/>
+          <Route path='/list' element={<List token={token}/>}/>
+          <Route path='/orders' element={<Orders token={token}/>}/>
         </Routes>
         </div>
         </div>
