708824769b4e0fa92fa51e6ee325596667af13a3
Create_first_repo <gauravchoundiye123@gmail.com>
2025-06-01 18:51:19 +0530

    routes updates admin

--- Admin/frontend_admin/src/App.jsx
+++ Admin/frontend_admin/src/App.jsx
@@ -31,9 +31,9 @@ const App = () => {
         <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
         <Routes>
           <Route path='/admin' element={<Users token={token}/>}/>
-          <Route path='/add' element={<Add token={token}/>}/>
-          <Route path='/list' element={<List token={token}/>}/>
-          <Route path='/orders' element={<Orders token={token}/>}/>
+          <Route path='/admin/add' element={<Add token={token}/>}/>
+          <Route path='/admin/list' element={<List token={token}/>}/>
+          <Route path='/admin/orders' element={<Orders token={token}/>}/>
         </Routes>
         </div>
         </div>
